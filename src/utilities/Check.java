fa9f28694e59a47f0594894d0bc98b4008a2daa5
zachkiser <kiserzt@dukes.jmu.edu>
2022-10-23 20:02:13 -0400

    step 40

--- src/utilities/Check.java
+++ src/utilities/Check.java
@@ -26,4 +26,23 @@ public class Check
     }
     return false;
   }
+  
+  /**
+   * Check to see if an array of String objects contains a particular
+   * String.
+   * 
+   * @param haystack  The array of String objects to search through
+   * @param needle The String object to search for
+   * @return true if haystack contains needle; false otherwise
+   */
+  public static boolean forContainsIgnoreCase(final String[] haystack, final String needle)
+  {
+    if (haystack == null) return false;
+
+    for (int i=0; i<haystack.length; i++)
+    {
+      if (haystack[i].equalsIgnoreCase(needle)) return true;
+    }
+    return false;
+  }
 }
