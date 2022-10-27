fb5737d5e1a565beea5a1a9dc6c1882ee8dcde9b
AJ Bushman <bushmaaj@dukes.jmu.edu>
2022-10-27 14:02:05 -0400

    Format to satisfy Checkstyle

--- src/utilities/ComplexCalc.java
+++ src/utilities/ComplexCalc.java
@@ -1,69 +1,72 @@
 package utilities;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-
 /**
- * Calculator GUI
- * @author TeamD
- * @version 11/4/22
- * This work complies with the JMU Honor Code.
+ * Calculator GUI.
  *
+ * @author TeamD
+ * @version 11/4/22 This work complies with the JMU Honor Code.
  */
 
-public class ComplexCalc extends JDialog {
+public class ComplexCalc extends JDialog
+{
 
-	/**
-	 * Constructor.
-	 */
-	public ComplexCalc() {
-		super();
-		setupLayout();
-	}
+  /**
+   * Constructor.
+   */
+  public ComplexCalc()
+  {
+    super();
+    setupLayout();
+  }
 
-	/**
-	 * Helper method.
-	 */
-	public void setupLayout() {
-		
-		// size of calculator
-		setSize(350, 200);
+  /**
+   * @param args
+   *     String array
+   */
+  public static void main(final String[] args)
+  {
+    ComplexCalc gui = new ComplexCalc();
+  }
 
-		Container contentPane;
-		contentPane = getContentPane();
-		contentPane.setLayout(new BorderLayout());
-		
-		// make panel to hold operator buttons
-	    JPanel p = new JPanel();
-	    p.setLayout(new GridLayout(1, 2));
-	    add(p, BorderLayout.SOUTH);
-	    JPanel operators = new JPanel();
-	    
-	    // adding buttons to panel
-	    operators.add(new JButton("R")); // reset
-	    operators.add(new JButton("C")); // clear
-	    operators.add(new JButton("+")); // addition
-	    operators.add(new JButton("-")); // subtraction
-	    operators.add(new JButton("x")); // multiplication
-	    operators.add(new JButton("/")); // division
-	    operators.add(new JButton("=")); // equals
-	    operators.setLayout((new FlowLayout(FlowLayout.TRAILING)));
-	    p.add(operators);
-	    
-	    setVisible(true);
-	}
+  /**
+   * Helper method.
+   */
+  public void setupLayout()
+  {
 
-	/**
-	 * @param args String array
-	 */
-	public static void main(final String[] args) {
-		ComplexCalc gui = new ComplexCalc();
-	}
+    // size of calculator
+    setSize(350, 200);
+
+    Container contentPane;
+    contentPane = getContentPane();
+    contentPane.setLayout(new BorderLayout());
+
+    // make panel to hold operator buttons
+    JPanel p = new JPanel();
+    p.setLayout(new GridLayout(1, 2));
+    add(p, BorderLayout.SOUTH);
+    JPanel operators = new JPanel();
+
+    // adding buttons to panel
+    operators.add(new JButton("R")); // reset
+    operators.add(new JButton("C")); // clear
+    operators.add(new JButton("+")); // addition
+    operators.add(new JButton("-")); // subtraction
+    operators.add(new JButton("x")); // multiplication
+    operators.add(new JButton("/")); // division
+    operators.add(new JButton("=")); // equals
+    operators.setLayout((new FlowLayout(FlowLayout.TRAILING)));
+    p.add(operators);
+
+    setVisible(true);
+  }
 }
 
