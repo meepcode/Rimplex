5f64b13e8df53691f77784fcdb2720196d8caa1b
nguye5st <nguye5st@dukes.jmu.edu>
2022-10-28 13:25:12 -0400

    Added getter method for textField to be accessed and parsed in outside class.

--- src/utilities/ComplexCalc.java
+++ src/utilities/ComplexCalc.java
@@ -101,58 +101,8 @@ public class ComplexCalc extends JFrame implements ActionListener
     // TODO Auto-generated method stub
 
   }
-
-  // public class ComplexCalc extends JDialog
-  // {
-  //
-  // /**
-  // * Constructor.
-  // */
-  // public ComplexCalc()
-  // {
-  // super();
-  // setupLayout();
-  // }
-  //
-  // /**
-  // * @param args
-  // * String array
-  // */
-  // public static void main(final String[] args)
-  // {
-  // ComplexCalc gui = new ComplexCalc();
-  // }
-  //
-  // /**
-  // * Helper method.
-  // */
-  // public void setupLayout()
-  // {
-  //
-  // // size of calculator
-  // setSize(350, 200);
-  //
-  // Container contentPane;
-  // contentPane = getContentPane();
-  // contentPane.setLayout(new BorderLayout());
-  //
-  // // make panel to hold operator buttons
-  // JPanel p = new JPanel();
-  // p.setLayout(new GridLayout(1, 2));
-  // add(p, BorderLayout.SOUTH);
-  // JPanel operators = new JPanel();
-  //
-  // // adding buttons to panel
-  // operators.add(new JButton("R")); // reset
-  // operators.add(new JButton("C")); // clear
-  // operators.add(new JButton("+")); // addition
-  // operators.add(new JButton("-")); // subtraction
-  // operators.add(new JButton("x")); // multiplication
-  // operators.add(new JButton("/")); // division
-  // operators.add(new JButton("=")); // equals
-  // operators.setLayout((new FlowLayout(FlowLayout.TRAILING)));
-  // p.add(operators);
-  //
-  // setVisible(true);
-  // }
+  
+  public String getTextField() {
+	  return this.textField.getText();
+  }
 }
