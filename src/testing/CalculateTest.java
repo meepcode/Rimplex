c1208d18541d2caae3f350cc7a03e0fafbfd5d7e
teemw <Zyiesha Hargro>
2022-11-02 13:20:52 -0400

    commit

---
+++ src/testing/CalculateTest.java
@@ -0,0 +1,27 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+class CalculateTest {
+	private final
+
+	@Test
+	void addTest() {
+
+	}
+
+	@Test
+	void subtractTest() {
+
+	}
+
+	@Test
+	void multiplyTest() {
+
+	}
+
+	@Test
+	void divideTest() {
+
+	}
+}
