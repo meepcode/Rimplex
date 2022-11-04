42de5262d3c34731b69116d1285d9774a3e22616
teemw <Zyiesha Hargro>
2022-11-04 11:53:17 -0400

    hey

--- src/testing/ComplexNumberTest.java
+++ src/testing/ComplexNumberTest.java
@@ -18,10 +18,18 @@ class ComplexNumberTest
   // even even
   private final double real2 = 10.0;
   private final double imag2 = 8.0;
+  
+  // negatives 
+  private final double realN = -4.0;
+  private final double imagN = -11.0;
 
   private ComplexNumber test = new ComplexNumber(real, imag);
   private ComplexNumber test1 = new ComplexNumber(real1, imag1);
   private ComplexNumber test2 = new ComplexNumber(real2, imag2);
+  
+  // negative imaginary part 
+  private ComplexNumber negI = new ComplexNumber(real, imagN);
+  // both negative real and imaginary 
 
   // trying to make one incorrectly
   // zeros???
@@ -37,11 +45,13 @@ class ComplexNumberTest
     assertEquals(3.0, test.getReal());
     assertEquals(5.0, test1.getReal());
     assertEquals(10.0, test2.getReal());
-    // getImaginary 
+    // getImaginary()
     assertEquals(2.0, test.getImaginary());
     assertEquals(7.0, test1.getImaginary());
     assertEquals(8.0, test2.getImaginary());
-
+    // toString()
+    String sTest = String.format("");
+    // assertEquals("", );
   }
 
 }
