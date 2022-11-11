5fffe3918f804f6d906e2685eef2c6be026f401e
nguye5st <nguye5st@dukes.jmu.edu>
2022-11-11 14:03:31 -0500

    Added menu items within menus

--- src/gui/MenuBar.java
+++ src/gui/MenuBar.java
@@ -2,19 +2,36 @@ package gui;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class MenuBar
+public class MenuBar extends JFrame
 {
  
   JMenuBar menuBar;
   JMenu file, edit, help;
+  JMenuItem print, exit, lang, mode, about;
   
   public MenuBar() {
     
     menuBar = new JMenuBar();
     
+    // file menu along menubar
     file = new JMenu("File");
+    print = new JMenuItem("Print");
+    exit = new JMenuItem("Exit");
+    menuBar.add(file);
+    
+    // edit menu along menubar
     edit = new JMenu("Edit");
+    lang = new JMenuItem("Language");
+    mode = new JMenuItem("Mode");
+    menuBar.add(edit);
+    
+    // help menu along menubar
     help = new JMenu("Help");
+    about = new JMenuItem("About");
+    menuBar.add(help);
+    
+    menuBar.setVisible(true);
   }
 }
