ec3404a7675be959a936ca208a65e431fac34f70
AJ Bushman <bushmaaj@dukes.jmu.edu>
2022-11-11 15:51:48 -0500

    Refactor Existing Parsing Classes

---
+++ src/calculation/parse/ExpressionEvaluationException.java
@@ -0,0 +1,18 @@
+package calculation.parse;
+
+/**
+ * Throws when an expression is formatted improperly.
+ */
+public class ExpressionEvaluationException extends Exception
+{
+  /**
+   * Throws the exception with the message.
+   *
+   * @param message
+   *     the message to throw with
+   */
+  public ExpressionEvaluationException(final String message)
+  {
+    super(message);
+  }
+}
