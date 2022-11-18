226acfa1dc8b7a85b6cf4b85155385f8de1725a6
zachkiser <kiserzt@dukes.jmu.edu>
2022-11-17 23:53:52 -0500

    Remove print button

--- src/gui/ComplexCalc.java
+++ src/gui/ComplexCalc.java
@@ -22,7 +22,7 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.print.*;
+// import java.awt.print.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.print.PageFormat;
@@ -392,54 +392,8 @@ public class ComplexCalc extends JFrame implements ActionListener, KeyListener
       // file menu along menubar
       file = new JMenu("File");
       menuBar.add(file);
-
-      print = new JMenuItem("Print");
-      file.add(print);
-      file.addActionListener(this);
-      print.addActionListener(new ActionListener()
-      {
-        public void actionPerformed(final ActionEvent e)
-        {
-          PrinterJob job = PrinterJob.getPrinterJob();
-          PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
-          PageFormat pf = job.pageDialog(aset);
-          job.setPrintable(new Printable()
-          {
-            @Override public int print(Graphics g, PageFormat pf, int page) throws PrinterException
-            {
-
-              if (page > 0)
-              { /* We have only one page, and 'page' is zero-based */
-                return NO_SUCH_PAGE;
-              }
-
-              /* User (0,0) is typically outside the imageable area, so we must
-               * translate by the X and Y values in the PageFormat to avoid clipping
-               */
-              Graphics2D g2d = (Graphics2D) g;
-              g2d.translate(pf.getImageableX(), pf.getImageableY());
-
-              /* Now we perform our rendering */
-              g.drawString("Test the print dialog!", 100, 100);
-
-              /* tell the caller that this page is part of the printed document */
-              return PAGE_EXISTS;
-            }
-          }, pf);
-          boolean ok = job.printDialog(aset);
-          if (ok)
-          {
-            try
-            {
-              job.print(aset);
-            }
-            catch (PrinterException ex)
-            {
-              /* The job did not successfully complete */
-            }
-          }
-        }
-      });
+      
+      
 
       exit = new JMenuItem("Exit");
       file.add(exit);
