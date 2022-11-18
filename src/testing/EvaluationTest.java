1ac7060b2f4ed5a1587aa96adb3a190605a570e1
hargroze <hargroze@dukes.jmu.edu>
2022-11-17 20:51:59 -0500

    commit

---
+++ src/testing/EvaluationTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EvaluationTest
+{
+
+  @Test
+  void test()
+  {
+    assertEquals(6, 6);
+  }
+
+}
